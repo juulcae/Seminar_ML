51826ae2eb0553a179dcfa823169eb1352121281
juulcae <165284070+juulcae@users.noreply.github.com>
2024-03-28 15:23:50 +0100

    Update Dataprocessor.py
    TransformData added

--- src/Dataprocessor.py
+++ src/Dataprocessor.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 class DataProcessor:
 
@@ -16,8 +16,8 @@ class DataProcessor:
     def cleanData(self):
         data = self.data
 
-        if self.name == '2015-07.csv':
-            data = data.drop(data.index[0])
+        # if self.name == '2015-07.csv':        dit wordt nu gedaan door TransformData()
+        #    data = data.drop(data.index[0])
 
         if self.name == '2015-07.csv' or self.name == '2024-02.csv':
             data['sasdate'] = pd.to_datetime(data['sasdate'])
@@ -27,6 +27,7 @@ class DataProcessor:
             data_cleaned.reset_index(inplace=True)
 
             self.data = data_cleaned
+            self.TransformData()
 
         return self.data
 
@@ -48,5 +49,46 @@ class DataProcessor:
         y_test = data_cleaned_test[dependentVariable].values[0]
 
         return [x_train, y_train, x_test, y_test]
+
+    def TransformData(self): # missing values are not taken into account
+        data = self.data
+        
+        for column in data 
+            if column != 'sasdate': # skip 'sasdate' column
+                transformation = data[column].iloc[0] # check transformation code
+                data[column].drop(data.index[0]) # drop transformation code from the column
+                if transformation == 2: 
+                    # first differences
+                    data[column] = data[column].diff()
+                elif transformation == 3: 
+                    # second differences
+                    data[column] = (data[column].diff()).diff()
+                elif transformation == 4: 
+                    # logarithm
+                    data[column] = np.log(data[column])
+                elif transformation == 5: 
+                    # log first differences
+                    data[column] = (np.log(data[column])).diff()
+                elif transformation == 6: 
+                    # log second differences
+                    data[column] = ((np.log(data[column])).diff()).diff()
+                elif transformation == 7: # i'm not sure about this , chatgpt also talked about pct_change()
+                    # Compute the first part of the transformation: ((xt/xt-1) - 1)
+                    part1 = (data[column] / data[column].shift(1)) - 1
+                    # Compute the second part of the transformation: ((xt-1/xt-2) - 1)
+                    part2 = (data[column].shift(1) / data[column].shift(2)) - 1
+                    # Combine the two parts to get the final transformation
+                    data[column] = part1 - part2
+                else: 
+                    # no transformation
+                    data[column] = data[column]
+                
+        self.data = data 
+
+        return self.data
+
+
+
+        
         
     
